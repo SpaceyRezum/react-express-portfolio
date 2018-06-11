ee8278670462a99f114d90fb8abd7c1d9f7f38c9
Alexis Bellet <info@alexis-bellet.com>
2018-06-11 14:03:38 -0400

    chores: updated babel/webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
+const path = require("path");
+
 module.exports = {
     entry: "./entry.js",
     output: {
-        path: "/public/",
+        path: path.join(__dirname, "/dist"),
         filename: "bundle.js"
     },
     module: {
       loaders: [
         {
          test: /.jsx?$/,
-         loader: 'babel-loader',
          exclude: /node_modules/,
+         loader: 'babel-loader',
          query: {
            presets: ['react', 'es2015']
          }
